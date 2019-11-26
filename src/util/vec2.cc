ba80d0ea602b2d532e7ef9714c4fac35c49c0dee
Joe Fowler <scrumplesplunge@gmail.com>
2019-11-26 21:20:25 +0000

    Add a vec2 class.

---
+++ src/util/vec2.cc
@@ -0,0 +1,79 @@
+export module util.vec2;
+
+import <type_traits>;
+
+export template <typename T>
+struct vec2 {
+  constexpr vec2() = default;
+
+  template <typename X, typename Y,
+            typename = std::enable_if_t<
+                std::is_convertible_v<X, T> && std::is_convertible_v<Y, T>>>
+  constexpr vec2(X x, Y y) : x(x), y(y) {}
+
+  constexpr vec2(vec2&& other) = default;
+  constexpr vec2(const vec2& other) = default;
+  constexpr vec2& operator=(vec2&& other) = default;
+  constexpr vec2& operator=(const vec2& other) = default;
+
+  template <typename U>
+  constexpr explicit vec2(vec2<U> other) : x(other.x), y(other.y) {}
+
+  T x, y;
+};
+
+// Currently (2019-11-14) this exported deduction guide doesn't seem to work :(
+// I'll leave it here just in case it starts working with newer compilers.
+export template <typename T>
+vec2(T, T) -> vec2<T>;
+
+export template <typename T>
+constexpr auto operator+(vec2<T> a, vec2<T> b) {
+  return vec2<T>{a.x + b.x, a.y + b.y};
+}
+
+export template <typename T>
+constexpr auto operator-(vec2<T> a, vec2<T> b) {
+  return vec2<T>{a.x - b.x, a.y - b.y};
+}
+
+export template <typename S, typename T>
+constexpr auto operator*(S s, vec2<T> v) {
+  return vec2<T>{s * v.x, s * v.y};
+}
+
+export template <typename S, typename T>
+constexpr auto operator*(vec2<T> v, S s) {
+  return vec2<T>{v.x * s, v.y * s};
+}
+
+export template <typename T>
+constexpr bool operator==(vec2<T> a, vec2<T> b) {
+  return a.x == b.x && a.y == b.y;
+}
+
+export template <typename T>
+constexpr bool operator<(vec2<T> a, vec2<T> b) {
+  if (a.y < b.y) return true;
+  if (a.y > b.y) return false;
+  return a.x < b.x;
+}
+
+export template <typename T>
+constexpr bool operator!=(vec2<T> a, vec2<T> b) { return !(a == b); }
+
+export template <typename T>
+constexpr bool operator>(vec2<T> a, vec2<T> b) { return b < a; }
+
+export template <typename T>
+constexpr bool operator<=(vec2<T> a, vec2<T> b) { return !(b < a); }
+
+export template <typename T>
+constexpr bool operator>=(vec2<T> a, vec2<T> b) { return !(a < b); }
+
+export template <typename T>
+struct std::hash<vec2<T>> {
+  constexpr auto operator()(vec2<T> v) const {
+    return (v.x * 19) ^ (v.y * 37);
+  }
+};
