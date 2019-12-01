15cbb9c7e03d327fea4fd525169afb6c33b489b6
Joe Fowler <scrumplesplunge@gmail.com>
2019-12-01 07:45:56 +0000

    Day 1.

---
+++ src/day1.cc
@@ -0,0 +1,22 @@
+import "util/check.h";
+import<fstream>;
+import<iostream>;
+
+int fuel(int mass) {
+  int x = mass / 3 - 2;
+  return x > 0 ? x + fuel(x) : 0;
+}
+
+int main(int argc, char* argv[]) {
+  check(argc == 2);
+  std::ifstream input(argv[1]);
+  check(input.good());
+  int part_1 = 0, part_2 = 0;
+  int x;
+  while (input >> x) {
+    part_1 += x / 3 - 2;
+    part_2 += fuel(x);
+  }
+  check(input.eof());
+  std::cout << "part1 " << part_1 << "\npart2 " << part_2 << "\n";
+}
