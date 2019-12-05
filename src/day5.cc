0c6eaebcdba48581e63d1ae2d456b33d74c308f1
Joe Fowler <scrumplesplunge@gmail.com>
2019-12-05 20:27:46 +0000

    Optimized version.

--- src/day5.cc
+++ src/day5.cc
@@ -6,12 +6,15 @@ import io;
 
 constexpr int max_program_size = 1000;
 
-enum class mode {
+enum class mode : unsigned char {
   position = 0,
   immediate = 1,
 };
 
-enum class opcode {
+constexpr bool is_mode(int x) { return 0 <= x && x <= 1; }
+
+enum class opcode : unsigned char {
+  illegal = 0,
   add = 1,
   mul = 2,
   input = 3,
@@ -23,24 +26,73 @@ enum class opcode {
   halt = 99,
 };
 
+constexpr bool is_opcode(int x) { return (1 <= x && x <= 8) || x == 99; }
+
+constexpr int op_size(opcode o) {
+  switch (o) {
+    case opcode::illegal: return -1;
+    case opcode::add:
+    case opcode::mul:
+    case opcode::less_than:
+    case opcode::equals:
+      return 4;
+    case opcode::jump_if_true:
+    case opcode::jump_if_false:
+      return 3;
+    case opcode::input:
+    case opcode::output:
+      return 2;
+    case opcode::halt:
+      return 1;
+  }
+}
+
 struct op {
-  opcode code;
+  opcode code = opcode::illegal;
   mode params[3];
 };
 
-op parse_op(int x) {
-  op result;
-  result.code = opcode(x % 100);
-  x /= 100;
-  for (int i = 0; i < 3; i++) {
-    mode param = result.params[i] = mode(x % 10);
-    // If this check fails, we have unsupported parameter modes.
-    check(param == mode::position || param == mode::immediate);
-    x /= 10;
-  }
-  // If this check fails, we have more parameter modes than any opcode uses.
-  check(x == 0);
-  return result;
+constexpr auto ops = [] {
+  constexpr auto parse_op = [](int x) -> op {
+    op result{};
+    int code = x % 100;
+    if (!is_opcode(code)) return {};
+    result.code = opcode(code);
+    x /= 100;
+    for (int i = 0; i < 3; i++) {
+      if (!is_mode(x % 10)) return {};
+      result.params[i] = mode(x % 10);
+      x /= 10;
+    }
+    // If this check fails, we have more parameter modes than any opcode uses.
+    if (x != 0) return {};
+    switch (result.code) {
+      case opcode::add:
+      case opcode::mul:
+      case opcode::less_than:
+      case opcode::equals:
+        if (result.params[2] == mode::immediate) return {};
+        break;
+      case opcode::input:
+        if (result.params[0] == mode::immediate) return {};
+        break;
+      case opcode::illegal:
+      case opcode::halt:
+      case opcode::jump_if_false:
+      case opcode::jump_if_true:
+      case opcode::output:
+        break;
+    }
+    return result;
+  };
+  std::array<op, 9999> ops = {};
+  for (int i = 0, n = ops.size(); i < n; i++) ops[i] = parse_op(i);
+  return ops;
+}();
+
+op decode_op(int x) {
+  check(0 <= x && x < (int)ops.size());
+  return ops[x];
 }
 
 int run(std::span<const int> program, std::optional<int> input) {
@@ -50,32 +102,32 @@ int run(std::span<const int> program, std::optional<int> input) {
   int pc = 0;
   while (true) {
     check(0 <= pc && pc < (int)program.size());
-    const auto op = parse_op(a[pc]);
+    const auto op = decode_op(a[pc]);
+    check(pc + op_size(op.code) <= (int)program.size());
     auto get = [&](int param_index) {
-      check(pc + param_index + 1 < (int)program.size());
       int x = a[pc + param_index + 1];
       switch (op.params[param_index]) {
-        case mode::position:
-          check(0 <= x && x < (int)program.size());
-          return a[x];
-        case mode::immediate:
-          return x;
+        case mode::position: return a[x];
+        case mode::immediate: return x;
       }
       assert(false);
     };
     auto put = [&](int param_index, int value) {
-      check(pc + param_index + 1 < (int)program.size());
       int x = a[pc + param_index + 1];
-      check(op.params[param_index] == mode::position);
       a[x] = value;
     };
     switch (op.code) {
+      case opcode::illegal:
+        std::cerr << "illegal instruction " << a[pc] << " at pc=" << pc << "\n";
+        std::abort();
       case opcode::add:
-      case opcode::mul: {
-        put(2, op.code == opcode::add ? get(0) + get(1) : get(0) * get(1));
+        put(2, get(0) + get(1));
         pc += 4;
         break;
-      }
+      case opcode::mul:
+        put(2, get(0) * get(1));
+        pc += 4;
+        break;
       case opcode::input:
         check(input);
         put(0, *input);
@@ -120,6 +172,8 @@ int main(int argc, char* argv[]) {
     (scanner >> exact(",") >> values[n++]).check_ok();
   }
   const std::span program(begin(values), n);
+  // To benchmark: bin/day5 puzzles/day5benchmark.txt
+  // check(run(program, 100'000'000) == 42);
   std::cout << "part1 " << run(program, 1) << '\n'
             << "part2 " << run(program, 5) << '\n';
 }
