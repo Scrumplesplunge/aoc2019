77e198a965b5a5e6b698436e21a7a61eb64d5c1c
Joe Fowler <scrumplesplunge@gmail.com>
2019-12-14 06:31:31 +0000

    Add two checks and some comments.

--- src/day14.cc
+++ src/day14.cc
@@ -17,6 +17,8 @@ struct reaction {
   std::string_view output_type;
   int output_quantity;
   std::map<std::string_view, int> requirements;
+  // The stage is the longest dependency sequence between this element and ORE.
+  // It is 1 for ORE itself, 2 for something immediately made from ORE, etc.
   std::optional<int> stage;
 };
 
@@ -41,9 +43,13 @@ constexpr std::int64_t ceil_div(std::int64_t a, std::int64_t b) {
   return (a + b - 1) / b;
 }
 
+// Calculate the minimum amount of ore needed to make the given amount of fuel.
 std::int64_t ore(const std::map<std::string_view, reaction>& reactions,
                  std::int64_t fuel) {
   std::map<std::string_view, std::int64_t> required = {{"FUEL", fuel}};
+  // Start at FUEL and work backwards in stage order. By doing it according to
+  // stage we cover the case where one dependent of a given reaction can be
+  // consumed as part of the construction of another dependent.
   for (int i = *reactions.at("FUEL").stage; i > 1; i--) {
     std::map<std::string_view, std::int64_t> new_required;
     for (const auto& [type, amount] : required) {
@@ -70,9 +76,13 @@ std::int64_t ore(const std::map<std::string_view, reaction>& reactions,
   return required.at("ORE");
 }
 
+// Binary search for the maximum amount of fuel that can be made from the
+// supplied amount of ore.
 std::int64_t max_fuel(const std::map<std::string_view, reaction>& reactions,
                       std::int64_t supplied_ore) {
   std::int64_t a = 0, b = 10'000'000'000'000;
+  check(ore(reactions, a) < supplied_ore);
+  check(ore(reactions, b) > supplied_ore);
   while (b - a > 1) {
     auto fuel = a + (b - a) / 2;
     auto required_ore = ore(reactions, fuel);
@@ -94,6 +104,7 @@ int main(int argc, char* argv[]) {
     reactions.emplace(reaction.output_type, std::move(reaction));
   }
 
+  // Compute the stage for each reaction.
   bool missing_stage;
   do {
     missing_stage = false;
