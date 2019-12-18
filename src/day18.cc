72b701ece9eb98e78a608609c1d1ecfee5ea451a
Joe Fowler <scrumplesplunge@gmail.com>
2019-12-18 08:16:41 +0000

    Day 2 initial solution. Good god, it's awful.

--- src/day18.cc
+++ src/day18.cc
@@ -28,6 +28,7 @@ struct state4 {
   std::array<vec2b, 4> positions = {};
   std::array<bool, 26> keys = {};
   int distance = 0;
+  int cost = 0;
 };
 
 constexpr bool is_key(char c) { return is_lower(c); }
@@ -68,17 +69,18 @@ generator<state> explore_unlocked(const grid& grid, const state& start) {
   //          << "].\n";
 }
 
-vec2b find_player(const grid& grid) {
+vec2b find(const grid& grid, char c) {
   for (int y = 0; y < grid_height; y++) {
     for (int x = 0; x < grid_width; x++) {
-      if (grid[y][x] == '@') return {x, y};
+      if (grid[y][x] == c) return {x, y};
     }
   }
-  std::cerr << "No player found in grid.\n";
+  std::cerr << "No '" << c << "' found in grid.\n";
   std::abort();
 }
 
-int num_keys(const state& state) {
+template <typename T>
+int num_keys(const T& state) {
   int keys = 0;
   for (bool k : state.keys) {
     if (k) keys++;
@@ -99,7 +101,7 @@ int num_keys(const grid& grid) {
 constexpr auto by_distance = [](const auto& l, const auto& r) {
   return l.distance > r.distance;
 };
-
+//
 //int part1(const grid& grid) {
 //  const int grid_keys = num_keys(grid);
 //  std::cout << grid_keys << " keys total.\n";
@@ -122,38 +124,126 @@ constexpr auto by_distance = [](const auto& l, const auto& r) {
 //  }
 //}
 
+struct heuristic {
+  heuristic(const grid& grid) {
+    for (int i = 0; i < 26; i++) keys[i] = find(grid, 'a' + i);
+  }
+
+  //int operator()(const state4& state) const {
+  //  int total = state.distance;
+  //  for (int i = 0; i < 26; i++) {
+  //    int min_additional = 0;
+  //    if (state.keys[i]) {
+  //      int best = 999'999;
+  //      for (vec2b v : state.positions) {
+  //        int distance = (keys[i] - v).manhattan_length();
+  //        if (distance < best) best = distance;
+  //      }
+  //      for (vec2b v : keys) {
+  //        int distance = (keys[i] - v).manhattan_length();
+  //        if (distance > 0 && distance < best) best = distance;
+  //      }
+  //      if (best > min_additional) min_additional = best;
+  //    }
+  //    total += min_additional;
+  //  }
+  //  return total;
+  //}
+
+  int operator()(const state4& state) const {
+    struct entry {
+      int distance = 0;
+      std::array<vec2b, 4> positions;
+      std::array<bool, 26> keys;
+    };
+    constexpr auto by_distance = [](const entry& l, const entry& r) {
+      return l.distance > r.distance;
+    };
+    std::priority_queue<entry, std::vector<entry>, decltype(by_distance)>
+        frontier;
+    frontier.push(entry{0, state.positions, state.keys});
+    std::set<std::array<vec2b, 4>> explored;
+    while (true) {
+      check(!frontier.empty());
+      auto current = frontier.top();
+      frontier.pop();
+      if (!explored.insert(current.positions).second) continue;
+      if (num_keys(current) == 26) return state.distance + current.distance;
+      for (int i = 0; i < 26; i++) {
+        if (current.keys[i]) continue;
+        for (int j = 0; j < 4; j++) {
+          auto next = current;
+          next.keys[i] = true;
+          next.positions[j] = keys[i];
+          next.distance += (keys[i] - current.positions[j]).manhattan_length();
+          if (!explored.count(next.positions)) frontier.push(next);
+        }
+      }
+    }
+  }
+
+  std::array<vec2b, 26> keys = {};
+};
+
 int part2(grid grid) {
   const int grid_keys = num_keys(grid);
-  std::cout << grid_keys << " keys total.\n";
-  vec2b origin = find_player(grid);
-  std::set<std::pair<std::array<vec2b, 4>, std::array<bool, 26>>> explored;
+  //std::cout << grid_keys << " keys total.\n";
+  auto [x, y] = find(grid, '@');
+  grid[y - 1][x - 1] = grid[y - 1][x + 1] = '@';
+  grid[y + 1][x - 1] = grid[y + 1][x + 1] = '@';
+  grid[y - 1][x] = grid[y][x] = grid[y + 1][x] = '#';
+  grid[y][x - 1] = grid[y][x + 1] = '#';
+  for (int y = 37; y <= 43; y++) {
+    for (int x = 35; x <= 45; x++) {
+      std::cout << grid[y][x];
+    }
+    std::cout << '\n';
+  }
+  std::set<std::tuple<std::array<vec2b, 4>, std::array<bool, 26>>> explored;
+  //heuristic heuristic(grid);
+  auto new_state4 = [&](const std::array<vec2b, 4>& positions,
+                        const std::array<bool, 26>& keys,
+                        int distance) {
+    state4 state{positions, keys, distance};
+    //state.cost = heuristic(state);
+    return state;
+  };
+  //constexpr auto by_cost = [](const state4& l, const state4& r) {
+  //  return l.cost > r.cost;
+  //};
   std::priority_queue<state4, std::vector<state4>, decltype(by_distance)>
       frontier;
-  frontier.push({
-      {{{origin.x - 1, origin.y - 1}, {origin.x + 1, origin.y - 1},
-        {origin.x - 1, origin.y + 1}, {origin.x + 1, origin.y + 1}}},
-      {}, 0});
+  frontier.push(new_state4(
+      {{{x - 1, y - 1}, {x + 1, y - 1}, {x - 1, y + 1}, {x + 1, y + 1}}},
+      {}, 0));
   long count = 0;
   while (true) {
     check(!frontier.empty());
     state4 current = frontier.top();
     frontier.pop();
+    if (num_keys(current) == grid_keys) return current.distance;
     if (++count % 1'000 == 0) {
       std::cout << "\riterations: " << count << ", explored: "
                 << explored.size() << ", distance: " << current.distance
                 << "  " << std::flush;
+//                << ", cost: " << heuristic(current) << "  " << std::flush;
     }
-    bool found = false;
-    for (auto i = explored.lower_bound({current.positions, {}});
-         i != explored.end() && i->first == current.positions; ++i) {
-      bool has_all = true;
-      for (int j = 0; j < 26; j++) {
-        if (current.keys[j] && !i->second[j]) has_all = false;
-      }
-      if (has_all) found = true;
+    if (!explored.emplace(current.positions, current.keys).second) {
+      continue;
     }
-    if (found) continue;
-    explored.emplace(current.positions, current.keys);
+    //bool found = false;
+    //for (auto i = explored.lower_bound(
+    //         {current.positions, keys_found, {}});
+    //     i != explored.end() && std::get<0>(*i) == current.positions; ++i) {
+    //  bool has_all = true;
+    //  const auto& keys = std::get<2>(*i);
+    //  for (int j = 0; j < 26; j++) {
+    //    if (current.keys[j] && !keys[j]) has_all = false;
+    //  }
+    //  if (has_all) found = true;
+    //}
+    //if (found) continue;
+    //explored.emplace(current.positions, keys_found, current.keys);
     //auto [x, y] = current.position;
     //std::cout << "reached " << grid[y][x] << " ["
     //          << (int)current.position.x << ", "
@@ -161,9 +251,9 @@ int part2(grid grid) {
     for (int i = 0; i < 4; i++) {
       state start = {current.positions[i], current.keys, current.distance};
       for (auto& next : explore_unlocked(grid, start)) {
-        if (num_keys(next) == grid_keys) return next.distance;
         state4 combined = {current.positions, next.keys, next.distance};
         combined.positions[i] = next.position;
+        //combined.cost = heuristic(combined);
         frontier.push(combined);
       }
     }
@@ -179,5 +269,7 @@ int main(int argc, char* argv[]) {
   (scanner >> scanner::end).check_ok();
   
   //std::cout << "part1 " << part1(grid) << "\n";
+  //wa 4722 (probably garbage, says too high)
+  //wa 1716 (too high)
   std::cout << "part2 " << part2(grid) << "\n";
 }
