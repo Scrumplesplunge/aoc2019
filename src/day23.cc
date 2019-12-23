4a65b7f6aefaaa04037bf2e5f4fb1754f475dbc9
Joe Fowler <scrumplesplunge@gmail.com>
2019-12-23 08:00:55 +0000

    Cleanup.

--- src/day23.cc
+++ src/day23.cc
@@ -16,11 +16,14 @@ using vec2v = vec2<program::value_type>;
 struct network;
 
 struct computer {
-  bool running = false;
   program cpu;
   program::state state = program::ready;
   circular_buffer<program::value_type, 1000> buffered_input;
   circular_buffer<program::value_type, 5> buffered_output;
+
+  bool running() {
+    return !(state == program::waiting_for_input && buffered_input.empty());
+  }
 };
 
 struct network {
@@ -32,19 +35,19 @@ struct network {
     auto& target = computers[destination];
     target.buffered_input.push(x);
     target.buffered_input.push(y);
-    target.running = true;
   }
 
+  // Run computer[i] until it gets blocked. A computer is blocked if it needs
+  // input but there is none, if it sends a message to the NAT via address 255,
+  // or if it halts. The question says that input should not block, but this is
+  // handled in the part1() and part2() functions.
   std::optional<vec2v> run(int index) {
-    constexpr int steps = 10;
     auto& computer = computers[index];
-    computer.running = true;
-    for (int i = 0; i < steps; i++) {
+    while (true) {
       switch (computer.state) {
         case program::ready: break;
         case program::waiting_for_input:
           if (computer.buffered_input.empty()) {
-            computer.running = false;
             return std::nullopt;
           }
           computer.cpu.provide_input(computer.buffered_input.pop());
@@ -64,31 +67,22 @@ struct network {
           }
           break;
         case program::halt:
-          computer.running = false;
           return std::nullopt;
       }
       computer.state = computer.cpu.resume();
     }
-    return std::nullopt;
+  }
+
+  using state_counts = std::array<int, 4>;
+  state_counts state() const {
+    state_counts output = {};
+    for (auto& c : computers) output[c.state]++;
+    return output;
   }
 };
 
 int part1(network network) {
   while (true) {
-    std::array<bool, 50> running_before, running_after;
-    do {
-      for (int i = 0; i < 50; i++) {
-        running_before[i] = network.computers[i].running;
-      }
-      for (int i = 0; i < 50; i++) {
-        if (network.computers[i].running) {
-          if (auto result = network.run(i); result) return result->y;
-        }
-      }
-      for (int i = 0; i < 50; i++) {
-        running_after[i] = network.computers[i].running;
-      }
-    } while (running_before != running_after);
     for (int i = 0; i < 50; i++) {
       network.computers[i].buffered_input.push(-1);
       if (auto result = network.run(i); result) return result->y;
@@ -100,30 +94,17 @@ int part2(network network) {
   std::optional<program::value_type> previous_y;
   vec2v nat;
   while (true) {
-    std::array<bool, 50> running_before, running_after;
-    do {
-      for (int i = 0; i < 50; i++) {
-        running_before[i] = network.computers[i].running;
-      }
-      for (int i = 0; i < 50; i++) {
-        if (network.computers[i].running) {
-          if (auto result = network.run(i); result) nat = *result;
-        }
-      }
-      for (int i = 0; i < 50; i++) {
-        running_after[i] = network.computers[i].running;
-      }
-    } while (running_before != running_after);
-    if (running_after == std::array<bool, 50>{}) {
-      std::map<program::state, int> states;
-      for (auto& x : network.computers) states[x.state]++;
-      if (states[program::waiting_for_input] == 50) {
-        network.send(0, nat.x, nat.y);
-        if (previous_y && nat.y == *previous_y) return nat.y;
-        previous_y = nat.y;
-      }
+    if (std::none_of(network.computers.begin(), network.computers.end(),
+                     [](auto& c) { return c.running(); }) &&
+        network.state()[program::waiting_for_input] == 50) {
+      network.send(0, nat.x, nat.y);
+      if (previous_y && nat.y == *previous_y) return nat.y;
+      previous_y = nat.y;
     }
     for (int i = 0; i < 50; i++) {
+      // Allow at most one non-blocking read. This allows for forward progress
+      // but also gives us chance to inspect the state and decide whether the
+      // NAT should transmit anything.
       network.computers[i].buffered_input.push(-1);
       if (auto result = network.run(i); result) nat = *result;
     }
