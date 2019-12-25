d2c970f37be52b732e1eaca8ebbd078a71832680
Joe Fowler <scrumplesplunge@gmail.com>
2019-12-25 16:09:29 +0000

    Knock 50ms off day 19.

--- src/day19.cc
+++ src/day19.cc
@@ -3,54 +3,59 @@ import <array>;
 import <charconv>;  // bug
 import <optional>;  // bug
 import <span>;
+import <unordered_map>;
 import intcode;
 import util.io;
 import util.vec2;
 
-bool is_pulled(program::const_span source, vec2i position) {
+bool is_pulled(program program, vec2i position) {
   program::value_type input[] = {position.x, position.y}, output_buffer[1];
-  auto output = program(source).run(input, output_buffer);
+  auto output = program.run(input, output_buffer);
   check(!output.empty());
   return output[0];
 }
 
-int part1(program::const_span source) {
+int part1(const program& program) {
   int count = 0;
   for (int y = 0; y < 50; y++) {
-    for (int x = 0; x < 50; x++) {
-      count += is_pulled(source, {x, y});
+    int x = 0;
+    while (x < 50 && !is_pulled(program, {x, y})) x++;
+    const int start = x;
+    if (x < 50) {
+      do { x++; } while (is_pulled(program, {x, y}));
     }
+    count += x - start;
   }
   return count;
 }
 
-vec2i start_position(program::const_span source) {
+vec2i start_position(const program& program) {
   for (int y = 0; y < 100; y++) {
-    if (is_pulled(source, {99, y})) return {99, y};
+    if (is_pulled(program, {99, y})) return {99, y};
   }
   for (int x = 99; x >= 0; x--) {
-    if (is_pulled(source, {x, 99})) return {x, 99};
+    if (is_pulled(program, {x, 99})) return {x, 99};
   }
   std::cerr << "Can't find start position.\n";
   std::abort();
 }
 
-vec2i next(program::const_span source, vec2i p) {
+vec2i next(const program& program, vec2i p) {
   for (vec2i v : {p + vec2i(1, 0), p + vec2i(1, 1), p + vec2i(0, 1)}) {
-    if (is_pulled(source, v)) return v;
+    if (is_pulled(program, v)) return v;
   }
   std::cerr << "Can't find next position from (" << p.x << ", " << p.y
             << ").\n";
   std::abort();
 }
 
-int part2(program::const_span source) {
-  vec2i position = start_position(source);
+int part2(const program& program) {
+  vec2i position = start_position(program);
   while (true) {
-    position = next(source, position);
+    position = next(program, position);
     if (position.x < 99 || position.y < 99) continue;
     auto bottom_right = position + vec2i(-99, 99);
-    if (is_pulled(source, bottom_right)) break;
+    if (is_pulled(program, bottom_right)) break;
   }
   vec2i top_left = position + vec2i(-99, 0);
   return top_left.x * 10'000 + top_left.y;
@@ -60,6 +65,6 @@ int main(int argc, char* argv[]) {
   program::buffer program_buffer;
   const auto source = program::load(init(argc, argv), program_buffer);
 
-  std::cout << "part1 " << part1(source) << '\n';
-  std::cout << "part2 " << part2(source) << '\n';
+  std::cout << "part1 " << part1(program(source)) << '\n';
+  std::cout << "part2 " << part2(program(source)) << '\n';
 }
