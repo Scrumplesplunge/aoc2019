0745f3915b67748e67a25d88586271f2b885f2a2
Joe Fowler <scrumplesplunge@gmail.com>
2019-12-25 16:38:03 +0000

    Add running time to test.sh

--- test.sh
+++ test.sh
@@ -4,11 +4,16 @@ make -q opt || make opt
 for answer in answers/*.txt; do
   puzzle="puzzles/$(basename "$answer")"
   solver="bin/opt/$(basename -s '.txt' "$answer")"
+  begin="$(date +'%s%N')"
+  "$solver" "$puzzle" > /tmp/out
+  end="$(date +'%s%N')"
+  runtime_millis=$(((end - begin) / 1000000))
   printf "$solver..."
-  if "$solver" "$puzzle" | colordiff --color=yes "$answer" - > /tmp/diff; then
-    printf " \x1b[32mPASSED\x1b[0m\n"
+  if colordiff --color=yes "$answer" /tmp/out > /tmp/diff; then
+    printf " \x1b[32mPASSED\x1b[0m"
   else
-    printf " \x1b[31mFAILED\x1b[0m: (\x1b[31mwant\x1b[0m, \x1b[32mgot\x1b[0m)\n"
+    printf " \x1b[31mFAILED\x1b[0m: (\x1b[31mwant\x1b[0m, \x1b[32mgot\x1b[0m)"
     cat /tmp/diff
   fi
+  printf " in \x1b[33m%4dms\x1b[0m\n" "$runtime_millis"
 done
